0554e1fd11ad6af83c71587d9166fa9942f365e9
Qilqilll <aqilf612@gmail.com>
2024-03-08 12:49:51 +0700

    menampilkan array yang telah tersusun berurutan

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -40,3 +40,15 @@ void bubblesortArray() {
 		pass = pass + 1;
 	} while (pass <= n - 1);
 }
+
+void display() {
+	cout << endl;
+	cout << "================================" << endl;
+	cout << "Elemen Array yang telah tersusun" << endl;
+	cout << "================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << a[j] << endl;
+	}
+	cout << "jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
